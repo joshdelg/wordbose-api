0f2be95e9f1585a05a96049287715fd6b68a3c4c
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2020-04-21 21:44:45 -0700

    Modified sendToTranscribe function to enable data file creation workflow. onTranscribeDump function will date data from destBucket/data/{transcriptId} as well as the transcript from destBucket/{transcriptId} and make a dynamoDB call eventually. Necessary permissions for getting/putting objects in destination bucket as well as getting objects from the uploads bucket have been added to the iamRoleStatements block in serverless.yml. Don't forgot the /* at the end of each S3 resource! :laughing:

--- sendToTranscribe.js
+++ sendToTranscribe.js
@@ -1,28 +1,53 @@
 import AWS from "aws-sdk";
+import { v4 as uuidv4 } from "uuid";
+import moment from "moment";
 import createResponse from "./libs/response-lib";
 
 export async function handler(event, context) {
 
-  const destBucket = 'wordbose-destination-test';
-
   // Triggered by S3 event
   const bucketName = event.Records[0].s3.bucket.name;
-  const itemKey = event.Records[0].s3.object.key; // In the form 'private/file-name'
-  const fileName = itemKey.substring(8);
+  const itemKey = event.Records[0].s3.object.key; // In the form 'private/user-id/file-name'
+
+  // Parsing file name
+  const userId = itemKey.substring(8, itemKey.lastIndexOf('/'));
+  const fileName = itemKey.substring(itemKey.lastIndexOf('/') + 1);
+  const transcriptId = uuidv4();
 
   const transcribe = new AWS.TranscribeService();
+  const s3 = new AWS.S3();
 
-  const params = {
+  const transcribeJobParams = {
     LanguageCode: 'en-US',
     Media: {
       MediaFileUri: `s3://${bucketName}/${itemKey}`
     },
-    TranscriptionJobName: `${fileName}-transcript`,
-    OutputBucketName: destBucket
+    TranscriptionJobName: transcriptId,
+    OutputBucketName: process.env.destBucketName
+  };
+
+  const transcriptData = {
+    userId: userId,
+    transcriptId: transcriptId,
+    transcriptName: fileName.substring(0, fileName.lastIndexOf('.')),
+    fileName: fileName,
+    fileLocation: transcribeJobParams.Media.MediaFileUri,
+    date: moment()
+  };
+
+  const s3UploadParams = {
+    Body: Buffer.from(JSON.stringify(transcriptData)),
+    Bucket: process.env.destBucketName,
+    Key: `data/${transcriptId}.json`
   };
 
   try {
-    await transcribe.startTranscriptionJob(params).promise();
+    // Send file to Transcribe
+    await transcribe.startTranscriptionJob(transcribeJobParams).promise();
+
+    // Upload data file to S3
+    await s3.putObject(s3UploadParams).promise();
+
     return createResponse(200, JSON.stringify({status: true}));
   } catch (e) {
     return createResponse(500, JSON.stringify({status: false}));
