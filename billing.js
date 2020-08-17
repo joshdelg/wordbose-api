6adc85304c1d004f9965d42606f1550dc781584e
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2020-08-16 21:00:57 -0700

    Attempting to fix stripe payments

--- billing.js
+++ billing.js
@@ -4,6 +4,7 @@ import stripePkg from "stripe";
 
 export async function handler(event, context) {
     const stripe = stripePkg(process.env.stripeSecret);
+    console.log("Stripe Key", process.env.stripeSecret);
 
     const data = JSON.parse(event.body);
 
